a91f50b0deecf1df31cd5ed5186bc4af56cba42d
byteoasis <hexstatic@mailbox.org>
2018-01-29 23:37:20 +0100

    Fix Heroku deployment steps

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+#git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
+#wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+#mkdir -p /usr/local/lib /usr/local/bin
+#tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+#ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
